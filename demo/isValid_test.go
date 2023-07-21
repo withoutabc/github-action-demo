e37eea972dd463c06a64c9d8075f74d659f996d8
withoutabc <3065312562@qq.com>
2023-07-21 20:31:08 +0800

    tianjia

---
+++ demo/isValid_test.go
@@ -0,0 +1,24 @@
+package demo
+
+import "testing"
+
+func TestLengthSmall(t *testing.T) {
+	type test struct {
+		input string
+		want  bool
+	}
+	tests := map[string]test{
+		"test1": {input: "(())", want: true},
+		"test2": {input: "{(]]", want: false},
+		"test3": {input: "([])", want: true},
+	}
+	for name, tc := range tests {
+		got := isValid(tc.input)
+		t.Run(name, func(t *testing.T) {
+			if got != tc.want {
+				t.Errorf("expected:%d, got:%d %#v", tc.want, got, tc)
+				//	t.Errorf("expected:%d, got:%d", tc.want, got)
+			}
+		})
+	}
+}
